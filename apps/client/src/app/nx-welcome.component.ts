b932cfc617691f569cab6b60b11f7e8e96c8d3dd
Lap Tran <laptrc@gmail.com>
2025-03-11 22:47:12 +0700

    Update nx-welcome.component.ts

--- apps/client/src/app/nx-welcome.component.ts
+++ apps/client/src/app/nx-welcome.component.ts
@@ -432,7 +432,7 @@ import { CommonModule } from '@angular/common';
         <div id="welcome">
           <h1>
             <span> Hello there, </span>
-            Welcome client 👋
+            Welcome Lap 👋
           </h1>
         </div>
         <!--  HERO  -->
