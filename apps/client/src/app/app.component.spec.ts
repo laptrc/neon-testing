c1b07c51329c25ff917c595ec5198f776f61b17b
Lap Tran <laptrc@gmail.com>
2025-03-18 11:58:26 +0700

    chore: Update title (#9)

--- apps/client/src/app/app.component.spec.ts
+++ apps/client/src/app/app.component.spec.ts
@@ -16,7 +16,7 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Tasks');
+    expect(compiled.querySelector('h1')?.textContent).toContain('To-do');
   });
 
   it(`should have as title 'client'`, () => {
